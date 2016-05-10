786aef537970d040af670c35cf07099d5956b132
Leonardo Uieda <leouieda@gmail.com>
2016-05-10 13:26:56 -0300

    Clean up initial gallery plots

--- gallery/meshes_grids/prism_model.py
+++ gallery/meshes_grids/prism_model.py
@@ -11,14 +11,21 @@ from fatiando.vis import myv
 
 # Models in Fatiando are basically sequences of geometric elements from
 # fatiando.mesher
+# Each element can have as many physical properties as you want.
+# Here, we'll give the prisms a density and a magnetization vector.
 model = [
-    Prism(0, 1000, 0, 2000, 2000, 2500, {'density': 500}),
+    Prism(-1500, -500, -1500, -500, 1000, 2000,
+          {'magnetization': [3, 5, 4], 'density': 500}),
+    Prism(500, 1500, 1000, 2000, 500, 1500,
+          {'magnetization': [10, 2, 1], 'density': -250}),
     ]
 
-bounds = [-5000, 5000, -5000, 5000, 0, 5000]
-
+# Create a 3D figure in Mayavi
 myv.figure()
-myv.prisms(model)
+# Give it some prisms to plot. The color will be decided by the density.
+myv.prisms(model, prop='density')
+# It's useful to plot axes and a bounding box about the prism
+bounds = [-2500, 2500, -2500, 2500, 0, 2500]
 myv.axes(myv.outline(bounds))
 myv.wall_north(bounds)
 myv.wall_bottom(bounds)
