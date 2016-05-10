b0c9909366ff341e8c7f9ad368d09ba14b9a8555
Leonardo Uieda <leouieda@gmail.com>
2016-05-10 13:02:10 -0300

    Try to make travis push to tmp-docs
    Was chaining three conditionals in bash if. Guess it's not Python after
    all.

--- ci-tools/push-tmp-docs.sh
+++ ci-tools/push-tmp-docs.sh
@@ -14,38 +14,43 @@ CLONE_ARGS="--quiet --branch=$BRANCH --single-branch"
 
 echo "Preparing to push HTML to branch ${BRANCH} of ${USER}/${REPO}"
 
-if [ "$TRAVIS_PULL_REQUEST" == "true" ] && [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$PYTHON" == "2.7" ];
+if [ "$PYTHON" == "2.7" ] && [ "$TRAVIS_OS_NAME" == "linux" ];
 then
-    echo -e "Copying generated files."
-    cp -R doc/_build/html/ $HOME/keep
-    # Go to home and setup git
-    cd $HOME
-    git config --global user.email "leouieda@gmail.com"
-    git config --global user.name "Leonardo Uieda"
-    git config --global github.user "leouieda"
-    echo -e "Cloning ${USER}/${REPO}"
-    # Clone the project, using the secret token.
-    # Uses /dev/null to avoid leaking decrypted key.
-    git clone ${CLONE_ARGS} ${REPO_URL} deploy > /dev/null
-    cd deploy
-    # Move the old branch out of the way and create a new one:
-    echo -e "Create an empty ${BRANCH} branch"
-    git checkout ${BRANCH}
-    git branch -m ${BRANCH}-old
-    git checkout --orphan ${BRANCH}
-    # Delete all the files and replace with our good set
-    echo -e "Remove old files from previous builds"
-    git rm -rf .
-    cp -Rf $HOME/keep/. $HOME/deploy
-    # Remove the CNAME file because this is not the main website
-    rm CNAME
-    # add, commit and push files
-    echo -e "Add and commit changes"
-    git add -f .
-    git commit -m "Travis build $TRAVIS_BUILD_NUMBER. Triggered by $TRAVIS_COMMIT"
-    echo -e "Pushing..."
-    git push -fq origin ${BRANCH} > /dev/null
-    echo -e "Finished uploading generated files."
+    if [ "$TRAVIS_PULL_REQUEST" == "true" ];
+    then
+        echo -e "Copying generated files."
+        cp -R doc/_build/html/ $HOME/keep
+        # Go to home and setup git
+        cd $HOME
+        git config --global user.email "leouieda@gmail.com"
+        git config --global user.name "Leonardo Uieda"
+        git config --global github.user "leouieda"
+        echo -e "Cloning ${USER}/${REPO}"
+        # Clone the project, using the secret token.
+        # Uses /dev/null to avoid leaking decrypted key.
+        git clone ${CLONE_ARGS} ${REPO_URL} deploy > /dev/null
+        cd deploy
+        # Move the old branch out of the way and create a new one:
+        echo -e "Create an empty ${BRANCH} branch"
+        git checkout ${BRANCH}
+        git branch -m ${BRANCH}-old
+        git checkout --orphan ${BRANCH}
+        # Delete all the files and replace with our good set
+        echo -e "Remove old files from previous builds"
+        git rm -rf .
+        cp -Rf $HOME/keep/. $HOME/deploy
+        # Remove the CNAME file because this is not the main website
+        rm CNAME
+        # add, commit and push files
+        echo -e "Add and commit changes"
+        git add -f .
+        git commit -m "Travis build $TRAVIS_BUILD_NUMBER. Triggered by $TRAVIS_COMMIT"
+        echo -e "Pushing..."
+        git push -fq origin ${BRANCH} > /dev/null
+        echo -e "Finished uploading generated files."
+    else
+        echo "Not a pull request so not updating website."
+    fi
 else
     echo -e "Not updating website."
 fi
